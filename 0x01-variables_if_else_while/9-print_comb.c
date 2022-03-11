a016a14b63295d30c5789a83e9864f05ad1ccc0b
Dewinsy <mushdaisy71@gmail.com>
2022-03-11 23:57:22 +0300

    Prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,11 @@ int main(void)
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-
-        if (n != '9')
-        {
-            putchar(',');
-            putchar(' ');
-        }
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
