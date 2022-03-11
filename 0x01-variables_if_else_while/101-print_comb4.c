6ed370fe0f3f2b45b9bd2f52f5e1f8126d7bdc27
Dewinsy <mushdaisy71@gmail.com>
2022-03-12 00:27:33 +0300

    Prints all possible combinations of three-digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all possible combinations of two-digit numbers
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	int c, i, j;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+                {
+                	for (j = '0'; j <= '9'; j++)
+                	{
+				if (c < i && i < j)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(j);
+
+				if (c != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
