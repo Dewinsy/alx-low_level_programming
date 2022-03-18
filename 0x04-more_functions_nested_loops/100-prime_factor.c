6bbef94aa4b6d3e5b07f666de84ff2b1defea092
Dewinsy <mushdaisy71@gmail.com>
2022-03-18 10:50:19 +0300

    print the largest primefactor of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,18 @@
-/**
- * C program to find all prime factors of a given number
- */
-
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
 {
-    int i, j, num, isPrime, count;
-    num = 1231952;
+	unsigned long int i = 3, n = 612852475143;
 
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-                    printf("%d, ", i);
-      }  }
-    return (0);
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
