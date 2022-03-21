1b2793788b99b4045bdfa8fbf9738c823699fc6c
Dewinsy <mushdaisy71@gmail.com>
2022-03-22 00:22:03 +0300

    prints every other character of a string,  starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line.
- *
+ * @str: string
  * Return: Always 0.
  */
 void puts2(char *str)
