a4252ab12cebab13d86a53e9b63e2bc2dd7d9695
Dewinsy <mushdaisy71@gmail.com>
2022-03-24 23:44:45 +0300

    more pointer and arrays advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,31 @@
+
+   
 #include "main.h"
 
 /**
-* rot13 - encodes a string using rot 13
-* @s: The string to encode
-* Return: (s) string
-*/
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
+ */
 char *rot13(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		for (i = 0; i <= 52; i++)
 		{
-			s[i] += 13;
-		}
-
-		else
-		{
-			while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			if (*s == rot13[i])
 			{
-				s[i] -= 13;
+				*s = ROT13[i];
+				break;
 			}
 		}
-		i++;
+		s++;
 	}
-	return (s);
+	return (ptr);
 }
