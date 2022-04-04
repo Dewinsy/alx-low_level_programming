0cd8ba8ed7488ac052a8e96de3df84e08d3028c5
Dewinsy <mushdaisy71@gmail.com>
2022-04-04 23:57:52 +0300

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,31 +1,64 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _strdup - Entry point
- *@str: string we need to duplicate
+ * _strlen - Swaps integers wih pointers.
  *
- * Return: Return success.
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*(s + i) != '\0')
+	i++;
+
+return (i);
+}
+
+
+/**
+ * str_concat - Concatenates 2 strings..
+ *
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: Returns the created array.
 **/
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-char *a;
-int i, c;
 
-if (str == NULL)
-return (NULL);
+int i, j, size1, size2, totSize;
+char *ar;
 
-for (i = 0; str[i] != '\0'; i++)
-;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
 
-a = malloc(i * sizeof(*a) + 1);
-if (a == NULL)
-return (NULL);
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+totSize = (size1 + size2)+1;
+ar = malloc(totSize *sizeof(char));
 
-for (c = 0; c < i; c++)
-a[c] = str[c];
-a[c] = '\0';
+if (ar == NULL)
+	return (NULL);
+else
+{
+for (i = 0; i < size1; i++)
+	ar[i] = s1[i];
 
-return (a);
+for (j = 0; j < size2; j++)
+{
+	ar[i] = s2[j];
+	i++;
+}
+ar[i + 1] = '\0';
+return (ar);
+}
+
 }
